ccfa5eb40aa12c5783a78f8c40f64d8f8be2d035
vuduydang <vuduydang2000@gmail.com>
2020-09-28 21:35:29 +0700

    update migration add column reply_for - dangvd

--- database/migrations/2020_09_28_143029_add_reply_for_to_contacts_table.php
+++ database/migrations/2020_09_28_143029_add_reply_for_to_contacts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateColumnReplyForInContacts extends Migration
+class AddReplyForToContactsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class CreateColumnReplyForInContacts extends Migration
     public function up()
     {
         Schema::table('contacts', function (Blueprint $table) {
-            $table->integer('reply_for')->nullable()->after('id');
+            $table->unsignedBigInteger('reply_for')->nullable()->after('id');
         });
     }
 
