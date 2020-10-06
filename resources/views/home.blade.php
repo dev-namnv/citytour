5264cde30110b75ceda1b28dc7a9462cd4115580
Hải Dương <nguyenhaiduong251298@gmail.com>
2020-10-06 22:16:07 +0900

    edit profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,6 @@
 @section('title', 'Welcome')
 
 @section('content')
-    <section id="hero">
-        <div class="intro_title">
-            <h3 class="animated fadeInDown">{{ __('pages.home.section.title') }}</h3>
-            <p class="animated fadeInDown">{{ __('pages.home.section.desc') }}</p>
-            <a href="all_tours_list.html" class="animated fadeInUp button_intro">{{ __('button.view_tours') }}</a> <a href="single_tour.html" class="animated fadeInUp button_intro outline">{{ __('button.read_more') }}</a>
-        </div>
-    </section>
 
     <main>
         <div class="container margin_60">
--- resources/views/layouts/main/app.blade.php
+++ resources/views/layouts/main/app.blade.php
@@ -56,7 +56,13 @@
         <!-- Header -->
         @include('layouts.main.header')
         <!-- End Header -->
-
+    <section id="hero">
+        <div class="intro_title">
+            <h3 class="animated fadeInDown">{{ __('pages.home.section.title') }}</h3>
+            <p class="animated fadeInDown">{{ __('pages.home.section.desc') }}</p>
+            <a href="all_tours_list.html" class="animated fadeInUp button_intro">{{ __('button.view_tours') }}</a> <a href="single_tour.html" class="animated fadeInUp button_intro outline">{{ __('button.read_more') }}</a>
+        </div>
+    </section>
         @yield('content')
         <!-- End main -->
 
