daa25de4376ba64c9f78dd97781e7928541f304a
duydang <vuduydang2000@gmail.com>
2020-11-28 18:49:46 +0700

    update filter contacts

--- resources/views/Manager/contacts/index.blade.php
+++ resources/views/Manager/contacts/index.blade.php
@@ -79,6 +79,7 @@
                                 @endforeach
                             </tbody>
                         </table>
+                        {{$contacts->links()}}
                     </div>
                 </div>
             </div>
