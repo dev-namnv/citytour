02e1286821dd8d52f2bae6597ec036b7c8b624aa
Hai Duong <nguyenhaid98@yahoo.com>
2020-12-04 21:27:34 +0700

    update admin menu

--- resources/views/layouts/manager/header.blade.php
+++ resources/views/layouts/manager/header.blade.php
@@ -3,10 +3,34 @@
     <div class="container-fluid d-flex align-items-stretch justify-content-between">
         <!--begin::Header Menu Wrapper-->
         <div class="header-menu-wrapper header-menu-wrapper-left" id="kt_header_menu_wrapper">
+            <!--begin::Header Menu-->
+            <div id="kt_header_menu" class="header-menu header-menu-mobile header-menu-layout-default">
+                <!--begin::Header Nav-->
+                <ul class="menu-nav">
+                    <li class="menu-item ">
+                        <a href="javascript:;" class="menu-link menu-toggle">
+                            <span class="menu-text">Pages</span>
+                        </a>
+                    </li>
+                    <li class="menu-item ">
+                        <a href="{{route('account')}}" class="menu-link menu-toggle">
+                            <span class="menu-text">Users</span>
+                        </a>
+                    </li>
+                    <li class="menu-item ">
+                        <a href="javascript:;" class="menu-link menu-toggle">
+                            <span class="menu-text">Contact</span>
+                        </a>
+                    </li>
+                </ul>
+                <!--end::Header Nav-->
+            </div>
+            <!--end::Header Menu-->
         </div>
         <!--end::Header Menu Wrapper-->
         <!--begin::Topbar-->
         <div class="topbar">
+
             <!--begin::Search-->
             <div class="dropdown" id="kt_quick_search_toggle">
                 <!--begin::Toggle-->
