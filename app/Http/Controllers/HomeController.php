372e265a37c5f5f021bff8426bc036640bd5072c
Hai Duong <nguyenhaid98@yahoo.com>
2020-12-04 20:56:37 +0700

    refactor home blade

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Contacts\SendContactRequest;
+use App\Models\Tour;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,7 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $tour = Tour::where('active', '=', 1)->limit(13)->get()->toArray();
+        $tour = array_chunk($tour,9);
+        return view('home',['main' => $tour[0],'tour' => $tour[1]]);
     }
 
 
