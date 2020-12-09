6963aab69344c606ea4598c0476b9303c2728b09
duydang <vuduydang2000@gmail.com>
2020-12-09 22:53:45 +0700

    sort list tour - dangvd

--- app/Http/Controllers/Main/TourController.php
+++ app/Http/Controllers/Main/TourController.php
@@ -17,7 +17,7 @@ class TourController extends Controller
             ->with(['batches' => function ($q) {
                 $q->select()->where('batch','>',date('Y-m-d'));
             }])
-            ->orderBy('created_at','desc')
+            ->orderBy('id','desc')
             ->paginate(PAGINATION_TOUR);
         $categories = Category::query()->get();
         if ($param == 'list-grid') {
