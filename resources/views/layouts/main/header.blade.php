05d00cccf45d537704f560210072ebe6db12623f
NamNV <dev.namnv@gmail.com>
2020-12-10 16:50:13 +0700

    fix login popup - NamNV

--- resources/views/layouts/main/header.blade.php
+++ resources/views/layouts/main/header.blade.php
@@ -37,7 +37,7 @@
                         {{--If auth invalid--}}
                         @guest
                             <li>
-                                <a href="{{route('login')}}" id="access_link">Đăng nhập</a>
+                                <a href="#sign-in-dialog" id="access_link">Đăng nhập</a>
                             </li>
                             @endguest
                             </li>
