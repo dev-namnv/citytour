46c693a1757cebd112b720de7e6d3e22ed845219
Hai Duong <nguyenhaid98@yahoo.com>
2020-12-14 22:02:52 +0700

    user list draft

--- resources/views/layouts/manager/aside_menu.blade.php
+++ resources/views/layouts/manager/aside_menu.blade.php
@@ -111,8 +111,8 @@
             </div>
         </li>
         @if(auth()->user()->role == ADMIN)
-        <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
-            <a href="javascript:;" class="menu-link menu-toggle">
+            <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
+                <a href="javascript:;" class="menu-link menu-toggle">
                     <span class="svg-icon menu-icon">
                         <!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
                         <svg xmlns="http://www.w3.org/2000/svg"
@@ -128,30 +128,30 @@
                         </svg>
                         <!--end::Svg Icon-->
                     </span>
-                <span class="menu-text">Liên hệ</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="menu-submenu">
-                <i class="menu-arrow"></i>
-                <ul class="menu-subnav">
-                    <li class="menu-item menu-item-parent" aria-haspopup="true">
+                    <span class="menu-text">Liên hệ</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="menu-submenu">
+                    <i class="menu-arrow"></i>
+                    <ul class="menu-subnav">
+                        <li class="menu-item menu-item-parent" aria-haspopup="true">
                         <span class="menu-link">
                             <span class="menu-text">Applications</span>
                         </span>
-                    </li>
-                    <li class="menu-item" aria-haspopup="true">
-                        <a href="{{route('contacts.index')}}" class="menu-link">
-                            <i class="menu-bullet menu-bullet-line">
-                                <span></span>
-                            </i>
-                            <span class="menu-text">Danh sách</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-        <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
-            <a href="javascript:;" class="menu-link menu-toggle">
+                        </li>
+                        <li class="menu-item" aria-haspopup="true">
+                            <a href="{{route('contacts.index')}}" class="menu-link">
+                                <i class="menu-bullet menu-bullet-line">
+                                    <span></span>
+                                </i>
+                                <span class="menu-text">Danh sách</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
+                <a href="javascript:;" class="menu-link menu-toggle">
                     <span class="svg-icon menu-icon">
                         <!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
                         <svg xmlns="http://www.w3.org/2000/svg"
@@ -167,36 +167,75 @@
                         </svg>
                         <!--end::Svg Icon-->
                     </span>
-                <span class="menu-text">Bài viết</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="menu-submenu">
-                <i class="menu-arrow"></i>
-                <ul class="menu-subnav">
-                    <li class="menu-item menu-item-parent" aria-haspopup="true">
+                    <span class="menu-text">Bài viết</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="menu-submenu">
+                    <i class="menu-arrow"></i>
+                    <ul class="menu-subnav">
+                        <li class="menu-item menu-item-parent" aria-haspopup="true">
                         <span class="menu-link">
                             <span class="menu-text">Applications</span>
                         </span>
-                    </li>
-                    <li class="menu-item" aria-haspopup="true">
-                        <a href="{{route('articles.index')}}" class="menu-link">
-                            <i class="menu-bullet menu-bullet-line">
-                                <span></span>
-                            </i>
-                            <span class="menu-text">Danh sách</span>
-                        </a>
-                    </li>
-                    <li class="menu-item" aria-haspopup="true">
-                        <a href="{{route('article_categories.index')}}" class="menu-link">
-                            <i class="menu-bullet menu-bullet-line">
-                                <span></span>
-                            </i>
-                            <span class="menu-text">Danh mục bài viết</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </li>
+                        </li>
+                        <li class="menu-item" aria-haspopup="true">
+                            <a href="{{route('articles.index')}}" class="menu-link">
+                                <i class="menu-bullet menu-bullet-line">
+                                    <span></span>
+                                </i>
+                                <span class="menu-text">Danh sách</span>
+                            </a>
+                        </li>
+                        <li class="menu-item" aria-haspopup="true">
+                            <a href="{{route('article_categories.index')}}" class="menu-link">
+                                <i class="menu-bullet menu-bullet-line">
+                                    <span></span>
+                                </i>
+                                <span class="menu-text">Danh mục bài viết</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
+                <a href="javascript:;" class="menu-link menu-toggle">
+                    <span class="svg-icon menu-icon">
+                        <!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px"
+                             viewBox="0 0 24 24" version="1.1">
+                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <rect x="0" y="0" width="24" height="24"/>
+                                <rect fill="#000000" x="4" y="4" width="7" height="7" rx="1.5"/>
+                                <path
+                                    d="M5.5,13 L9.5,13 C10.3284271,13 11,13.6715729 11,14.5 L11,18.5 C11,19.3284271 10.3284271,20 9.5,20 L5.5,20 C4.67157288,20 4,19.3284271 4,18.5 L4,14.5 C4,13.6715729 4.67157288,13 5.5,13 Z M14.5,4 L18.5,4 C19.3284271,4 20,4.67157288 20,5.5 L20,9.5 C20,10.3284271 19.3284271,11 18.5,11 L14.5,11 C13.6715729,11 13,10.3284271 13,9.5 L13,5.5 C13,4.67157288 13.6715729,4 14.5,4 Z M14.5,13 L18.5,13 C19.3284271,13 20,13.6715729 20,14.5 L20,18.5 C20,19.3284271 19.3284271,20 18.5,20 L14.5,20 C13.6715729,20 13,19.3284271 13,18.5 L13,14.5 C13,13.6715729 13.6715729,13 14.5,13 Z"
+                                    fill="#000000" opacity="0.3"/>
+                            </g>
+                        </svg>
+                        <!--end::Svg Icon-->
+                    </span>
+                    <span class="menu-text">Người dùng</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="menu-submenu">
+                    <i class="menu-arrow"></i>
+                    <ul class="menu-subnav">
+                        <li class="menu-item menu-item-parent" aria-haspopup="true">
+                        <span class="menu-link">
+                            <span class="menu-text">Applications</span>
+                        </span>
+                        </li>
+                        <li class="menu-item" aria-haspopup="true">
+                            <a href="{{route('user.list')}}" class="menu-link">
+                                <i class="menu-bullet menu-bullet-line">
+                                    <span></span>
+                                </i>
+                                <span class="menu-text">Danh sách</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
         @endif
     </ul>
     <!--end::Menu Nav-->
