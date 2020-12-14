d3d4eaf62b6640ac44dc1339e2183b5cbf213652
Ngoc Anh <ngocanhphamk2@gmail.com>
2020-12-14 22:28:47 +0700

    Bỏ chức năng bình luận bài viết

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -45,11 +45,6 @@ class Article extends Model
         return $this->belongsToMany('App\Models\ArticleTag', 'relation_article_tag', 'article_id', 'tag_id');
     }
 
-    public function comments()
-    {
-        return $this->hasMany('App\Models\ArticleComment');
-    }
-
     public function user()
     {
         return $this->belongsTo(User::class);
