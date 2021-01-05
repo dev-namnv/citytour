9533d0fa1ed2490a8534d39e05829ebc807ed761
Ngoc Anh <ngocanhphamk2@gmail.com>
2021-01-05 20:24:22 +0700

    Add share button to tour detail and article detail

--- resources/views/layouts/main/app.blade.php
+++ resources/views/layouts/main/app.blade.php
@@ -46,21 +46,9 @@
 <body>
     <!-- Load Facebook SDK for JavaScript -->
     <div id="fb-root"></div>
-    <script>
-        window.fbAsyncInit = function() {
-            FB.init({
-                xfbml            : true,
-                version          : 'v9.0'
-            });
-        };
-
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
+    <script async defer crossorigin="anonymous"
+            src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v9.0&appId=579174529687859&autoLogAppEvents=1"
+            nonce="SD0vQZXv"></script>
 
     <!-- Your Chat Plugin code -->
     <div class="fb-customerchat"
